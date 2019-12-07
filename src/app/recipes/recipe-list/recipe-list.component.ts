70a8417d414474e7b85ce2bb6bcbd557acf15d2d
Nikola Vujic <nikvujic@gmail.com>
2019-12-08 00:44:26 +0100

    initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Orange juice', 'A simple orange juice',
+      'https://www.earthfoodandfire.com/wp-content/uploads/2018/04/Homemade-Orange-Juice.jpg'),
+    new Recipe('Boiled potato', 'Boiled potato with salt', 
+      'https://cdn.pixabay.com/photo/2014/07/15/00/04/potato-393563_960_720.jpg')    
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
